453baa6fcf77bdd58ce89d1e7f00608a6b8e9a4d
leejiseop <lkj0245@naver.com>
2023-01-19 11:44:30 +0900

    modify ItemRepository comments

--- src/main/java/com/example/secondteamproject/repository/ItemRepository.java
+++ src/main/java/com/example/secondteamproject/repository/ItemRepository.java
@@ -10,13 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.awt.print.Pageable;
 import java.util.List;
 
-//findAllDesc 삭제
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOrderByModifiedAtDesc();
-//
-//<<<<<<< HEAD
-//    // ScanIterator<Object> findAllDesc();
-//=======
-//>>>>>>> 4db9a6af46ce67cb6c1f8a4a311c92aa893d2031
 }
